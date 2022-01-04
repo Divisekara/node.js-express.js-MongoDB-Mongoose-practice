dde0503e995c381e6a195f25e42ed52b80d3702e
ASITHA INDRAJITH DIVISEKARA <46625806+Divisekara@users.noreply.github.com>
2022-01-05 02:01:47 +0530

    server is up and running correctly on port 3000

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const express = require(express);
-const mongoose = require(mongoose);
+//jshint es version:6
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.listen(3000);
+app.listen(3000, function() {
+    console.log("Server up and running on port 3000.");
+});
